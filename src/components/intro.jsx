3b67ebb09bd21d66c0bd5b4acca7b16458310bde
erxiaowu <erxiaowu@tencent.com>
2017-12-14 00:22:53 +0800

    change home page

--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -2,20 +2,17 @@ import React, { Component } from 'react';
 import { init } from 'ityped';
 import './intro.css';
 
-
-
-
 export default class Intro extends Component {
 	componentDidMount() {
-		init(`#element1`, {
-			strings: ['《得意忘形》是一个主张追求个体自由与探寻真理的媒体计划', '带着对生命的有限性与无目的性的敬畏', '我们试图为读者与听众提供更全面的觉察自我与认知世界的工具', '以不断重建当下的方式', '穿越时间、抵达生活的本质。'],
-			typeSpeed: 150, //default
-			backSpeed: 30, //default
-			startDelay: 500, //default
-			backDelay: 500, //default
-			loop: false, //default    
-			showCursor: true, //default    
-			cursorChar: "|", //default
+		init(`#ele1`, {
+			strings: ['《得意忘形》是一个主张追求个体自由与探寻真理的媒体计划', '带着对生命的有限性与无目的性的敬畏', '我们试图为读者与听众提供更全面的觉察自我与认知世界的工具', '以不断重建当下的方式穿越时间', '抵达生活的本质'],
+			typeSpeed: 180,
+			backSpeed: 60,
+			startDelay: 500,
+			backDelay: 2500,
+			loop: false,    
+			showCursor: true,    
+			cursorChar: "|",
 			onFinished: function(){}
 		});
 	}
@@ -23,7 +20,9 @@ export default class Intro extends Component {
 		
 		return (
 			<div className="intro">
-				<span id="element1" className="aniText"></span>
+				<div className="textCover">
+					<span id="ele1" className="aniText"></span>
+				</div>
 			</div>
 		)
 	}
---
+++ src/components/podcast-card.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './podcast-card.css';
+
+export default class PodcastCard extends Component {
+	render() {
+		return (
+			<div>
+				<h1>podcast</h1>
+			</div>
+		)
+	}
+}
